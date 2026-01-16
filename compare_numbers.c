346c0b9cfbc08ae0a97eecd7411ac20905139703
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-01-16 21:19:13 +0530

    added compare_numbers.c file
    program to compare two numbers by using ternary operator in c

---
+++ compare_numbers.c
@@ -0,0 +1,23 @@
+// Write a program to compare two no by using of ternary operator in c
+
+
+#include<stdio.h>
+
+int main()
+
+{
+
+Int a,b;
+
+printf("enter two number");
+
+scanf("%d%d",&a,&b);
+
+int greater =(a>b)?a:b;
+
+printf(“greater value is %d”,greater);
+//printf("greater no is%d",a>b?a:b);
+
+Return 0;
+
+}
