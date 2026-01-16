df1ebbc17751197366d8dc30dbf083e11d885551
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-01-16 20:05:17 +0530

    Add multi-line comment.c file
    program to explain the usage of multi line comments

---
+++ multi_line_comment.c
@@ -0,0 +1,15 @@
+// Multi-line Comments in C
+
+#include <stdio.h>
+
+int main() {
+
+/* The code below will print the words Hello World!
+
+to the screen, and it is amazing */
+
+printf("Hello World!");
+
+return 0;
+
+}
