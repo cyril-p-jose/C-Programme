4166d967ec5dc051f0cc04170b94768a0070a178
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-01-31 13:31:06 +0530

    Create pyramid.c

---
+++ patterns/pyramid.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, space;
+
+    for (i = 1; i <= 4; i++) {
+        for (space = 4; space > i; space--) {
+            printf(" ");
+        }
+        for (j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
