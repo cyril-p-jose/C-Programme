a0d6efc41477ebefe3f73dca543a332866574214
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-03-23 21:05:19 +0530

    Add sumArray function and main for testing
    Implement a function to sum elements of an array and test it in main.

---
+++ FUNCTIONS IN C/sum_array.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int sumArray(int arr[], int n)
+{
+    int i, sum = 0;
+
+    for(i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+int main()
+{
+    int arr[100], n, i, result;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    result = sumArray(arr, n);
+
+    printf("Sum = %d", result);
+
+    return 0;
+}
